522dbbe78812b306433755e6cff12b8efcf167be
Glauber <glauberlv@gmail.com>
2014-09-13 00:06:52 -0300

    Commit inicial - Teste

---
+++ Glauber.Teste_MVC.Dominio/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glauber.Teste_MVC.Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ Teste_MVC4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Teste_MVC4.MvcApplication" Language="C#" %>
